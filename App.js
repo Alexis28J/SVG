d3ced5faa13c9aeea64c48e080a857d0f7fbf058
Alexis28J <jordy2806@hotmail.com>
2025-12-10 13:01:03 +0100

    Fine lezione (10.12.2025)

---
+++ App.js
@@ -0,0 +1,7 @@
+//Un'immagine SVG ci permette di modificarla riscrivendo il suo codice html --  vedi l'immagine (visualizza su text editor)
+//fill = 'yellow' - ho colorato l'immagine di giallo
+
+
+//TASK
+//Creare un'icona hamburguer menu icon ('tre lineette') dentro un botone
+//che cambie di colore e di posizione a verticale
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SVG
+
+SVG element su Developer Mozilla  ---  Proprietà dei svg
+
+https://www.vectorpea.com/ - Vectorpea 
+Posso disegnare l'svg che voglio e poi copiare il codice di quello che disegnato.
